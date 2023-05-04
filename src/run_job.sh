a37ecdb7d9785ed0e19f26c6aa1802ad50eaf2d8
Olanrewaju Farooq <olanrewajufarooq@yahoo.com>
2023-05-04 16:55:08 +0200

    Updating the bash script for training MAEProb_Loss

--- src/run_job.sh
+++ src/run_job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --time=00:30:00
+#SBATCH --time=03:00:00
 #SBATCH --partition=mundus
 #SBATCH --gres=gpu:1
-#SBATCH --job-name=run_all_models_04_04
-#SBATCH --output=run_all_models_04_04.out
-#SBATCH --error=run_all_models_04_04.err
+#SBATCH --job-name=MAEProb_Loss
+#SBATCH --output=MAEProb_Loss.out
+#SBATCH --error=MAEProb_Loss.err
 
-python -m scripts.eval.snapshot /mundus/folanrewa525/workspace/DriftModelling daily2016_15 pretrained --channels 5
-
+python -m scripts.train.snapshot /mundus/folanrewa525/workspace/DriftModelling daily2016_15 MAEProb_Loss --channels 5 --loss MAEProbDistrLoss
+python -m scripts.eval.snapshot /mundus/folanrewa525/workspace/DriftModelling daily2016_15 MAEProb_Loss --channels 5
