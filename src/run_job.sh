3579867d4e18231feb8eeeba4e710648b487239e
Olanrewaju Farooq <olanrewajufarooq@yahoo.com>
2023-05-06 08:02:57 +0200

    Updating the bash script to add cuda constraint

--- src/run_job.sh
+++ src/run_job.sh
@@ -3,6 +3,7 @@
 #SBATCH --time=03:00:00
 #SBATCH --partition=mundus
 #SBATCH --gres=gpu:1
+#SBATCH --constraint:'cuda61'
 #SBATCH --job-name=MAEProb_Training
 #SBATCH --output=MAEProb_Training.out
 #SBATCH --error=MAEProb_Training.err
