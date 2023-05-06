85056235d6ae418d763b91f7046fac2d1850843c
Olanrewaju Farooq <olanrewajufarooq@yahoo.com>
2023-05-06 18:58:57 +0200

    Updating the bash script to train MSEProbDistrLoss and 0.2

--- src/run_training_job.sh
+++ src/run_training_job.sh
@@ -7,4 +7,4 @@
 #SBATCH --output=MAEProb_Training.out
 #SBATCH --error=MAEProb_Training.err
 
-python -m scripts.train.snapshot /mundus/folanrewa525/workspace/DriftModelling daily2016_15 MAEProb_Loss --channels 5 --loss MAE --lossalpha 0.4
+python -m scripts.train.snapshot /mundus/folanrewa525/workspace/DriftModelling daily2016_15 MAEProb_Loss --channels 5 --loss MSEProbDistrLoss --lossalpha 0.2
