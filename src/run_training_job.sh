e93a34dd11e3f0f83d4a24c5b1fbee411f687a8e
Olanrewaju Farooq <olanrewajufarooq@yahoo.com>
2023-05-06 14:28:46 +0200

    Updating the bash script to change the partition to all

--- src/run_training_job.sh
+++ src/run_training_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --time=03:00:00
-#SBATCH --partition=mundus
+#SBATCH --partition=all
 #SBATCH --gres=gpu:8
 #SBATCH --job-name=MAEProb_Training
 #SBATCH --output=MAEProb_Training.out
