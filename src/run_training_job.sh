422af8b50c66f73b62398dd156da251a65c82a8f
Olanrewaju Farooq <olanrewajufarooq@yahoo.com>
2023-05-06 20:40:00 +0200

    Updating bash code

--- src/run_training_job.sh
+++ src/run_training_job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --time=03:00:00
+#SBATCH --time=10:00:00
 #SBATCH --partition=mundus
-#SBATCH --gres=gpu:1
-#SBATCH --job-name=MAEProb_Training
-#SBATCH --output=MAEProb_Training.out
-#SBATCH --error=MAEProb_Training.err
+#SBATCH --gres=gpu:2
+#SBATCH --job-name=MAEProb_0.2_Training
+#SBATCH --output=MAEProb_0.2_Training.out
+#SBATCH --error=MAEProb_0.2_Training.err
 
 python -m scripts.train.snapshot /mundus/folanrewa525/workspace/DriftModelling daily2018_15 MAEProb_Loss --channels 5 --loss MSEProbDistrLoss --lossalpha 0.2
